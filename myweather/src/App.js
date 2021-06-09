f4dad7b8c4e011d6bcf9670d55728b4f3cf23783
autumn <892049513@qq.com>
2021-06-09 19:25:25 +0800

    添加了一点样式

--- myweather/src/App.js
+++ myweather/src/App.js
@@ -1,4 +1,5 @@
 
+import './App.css'
 import 'antd/dist/antd.css'
 import {useState, useEffect} from 'react'
 import Header from './components/Header/Header'
--- myweather/src/components/Header/Header.js
+++ myweather/src/components/Header/Header.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { IconName } from "react-icons/wi";
+import './Header.css'
+import {
+    AiOutlineDoubleLeft,
+    AiOutlineDashboard,
+    AiOutlineCloud,
+  } from "react-icons/ai";
+  import { FiCloudRain } from "react-icons/fi";
+  import {
+    WiThermometer,
+    WiStrongWind,
+    WiThermometerInternal,
+  } from "react-icons/wi";
 
 export default function Header(props) {
+ 
     return (
         <div className="header">
-            <p>{props.detail.temp}</p>
+            <span className="location">北京</span>
+            <span className="date">6月9日</span>
+            <div className="weather">
+                <span className="rain">{props.detail.temp}</span>
+                <div className="other">
+                    <div className="icon"> <FiCloudRain /></div>
+                    <div className="txt">
+                        <div className="title">降水量</div>
+                        <div className="value">{props.detail.precip}</div>
+                    </div>
+                   
+                </div>
+            </div>
         </div>
     )
 }
